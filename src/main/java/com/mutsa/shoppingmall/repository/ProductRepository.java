9045307640ecc3c0fcf7494e54843e53351390f8
danhandev <danhandev@gmail.com>
2025-07-09 13:32:50 +0900

    Revert "Merge pull request #3 from danhandev/feature/cart-add-item"
    This reverts commit 2ca4984ac369fc60f3fc27fe32261b9239445f80, reversing
    changes made to bef1264fcf6ee1790c2594c5d29f5d9349c001d4.

--- src/main/java/com/mutsa/shoppingmall/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mutsa.shoppingmall.repository;
-
-import com.mutsa.shoppingmall.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-} 
